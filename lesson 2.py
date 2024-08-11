e7e56b9f5af6b42e48d700bd34ef29a6ab1301d1
Iron-Bum <chasmonahtus@gmail.com>
2024-08-12 01:41:21 +0300

    Add files via upload

--- lesson 2.py
+++ lesson 2.py
@@ -1,4 +1,5 @@
 total_lessons = 12
 time_spend = 1.5
 course = 'Pyton'
-print('Курс' ,course, ',всего задач :',total_lessons,',затрачено часов :',time_spend,',среднее время выполнения',(time_spend/total_lessons),'часа.')
+task_time = time_spend/total_lessons
+print('Курс' ,course, ',всего задач :',total_lessons,',затрачено часов :',time_spend,',среднее время выполнения',task_time,'часа.')
