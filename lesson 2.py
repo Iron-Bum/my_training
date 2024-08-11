584d2c0845c2128fe90f8eac2a4e30cfc4bf9977
Iron-Bum <chasmonahtus@gmail.com>
2024-08-12 01:26:31 +0300

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,4 @@
+total_lessons = 12
+time_spend = 1.5
+course = 'Pyton'
+print('Курс' ,course, ',всего задач :',total_lessons,',затрачено часов :',time_spend,',среднее время выполнения',(time_spend/total_lessons),'часа.')
