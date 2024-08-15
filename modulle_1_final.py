8c49c7403845d6bc42e29de731e7bd12327fdafd
Iron-Bum <chasmonahtus@gmail.com>
2024-08-16 01:50:08 +0300

    Add files via upload

---
+++ modulle_1_final.py
@@ -0,0 +1,11 @@
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students={'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+students_sort=sorted(list(students))
+grades_sum=(sum(grades[0]),sum(grades[1]),sum(grades[2]),sum(grades[3]),sum(grades[4]))
+print(grades_sum)
+average_score=(grades_sum[0]/len(grades[0]),grades_sum[1]/len(grades[1]),grades_sum[2]/len(grades[2]),
+               grades_sum[3]/len(grades[3]),grades_sum[4]/len(grades[4]))
+print(average_score)
+print(students_sort)
+student_grades=dict(zip(students_sort,average_score))
+print(student_grades)
