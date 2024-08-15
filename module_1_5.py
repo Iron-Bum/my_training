5e4e92772fc2b5d73de859233a2a87d5d9b7e8cb
Iron-Bum <chasmonahtus@gmail.com>
2024-08-15 08:43:41 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,9 @@
 
-immutable_war = 2,5, "string", True #кортеж
+immutable_war = 1,[2,5,8],"string", True #кортеж
 print(immutable_war)
+immutable_war[1][2]=13
 #immutable_war[1]=7 Нельзя изменить значения элемента кортежа т.к. они неизменны!
-#print(immutable_war)
+print(immutable_war)
 mutable_list = [3,7,"name", False] #список
 print(mutable_list)
 mutable_list[2]= 'age' # Значения в списке можно изменять
