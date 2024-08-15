7897f06a4eaed0697c1ee2e9eae36c611bec0146
Iron-Bum <chasmonahtus@gmail.com>
2024-08-15 10:11:11 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -9,7 +9,7 @@ print(my_dict)
 
 my_set={1,3,23,3,0,23,7,1,2,3,15,'Max','Bill','Max'}
 print(my_set)
-my_set.update({35,'Lisa'})
+my_set.update({35,'Lisa'}) #нет возможности использовать метод .add , т.к. он может добавить только один элемент
 print(my_set)
 my_set.discard('Bill') #можно использовать метод ,remuve, но при отсутствии значения он будет выдавать ошибку
 print(my_set)
