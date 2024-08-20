9334bc8f53e034b70192102f57d1b6ba5d19e2da
Iron-Bum <chasmonahtus@gmail.com>
2024-08-20 20:08:53 +0300

    comit

---
+++ main.py
@@ -0,0 +1,7 @@
+example = 'yellow'
+print(example[0])# 1е значение , знак от которго идет отсчет, 2е значение-до какаого знако (срез), 3е - шаг(может быть отрицательным)
+print(example[-1])
+print(example[2:])
+print(example[::-1])
+print(example[1::2])
+print(example[4::-1])
