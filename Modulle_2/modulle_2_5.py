b4c85eb7a80a7b30395c8f01b11839879ade616c
Iron-Bum <chasmonahtus@gmail.com>
2024-08-21 21:34:58 +0300

    upd.

--- Modulle_2/modulle_2_5.py
+++ Modulle_2/modulle_2_5.py
@@ -6,5 +6,9 @@ def get_matrix(n, m, value):
             list_matrix.append(value)
         matrix.append(list_matrix)
     return matrix
-result1 = get_matrix(4, 5, 15)
+result1 = get_matrix(2, 2, 15)
+result2 = get_matrix(3, 5, 27)
+result3 = get_matrix(5, 3, 88)
 print(result1)
+print(result2)
+print(result3)
