ff72465556419ad0af7393be0da498def348c748
Iron-Bum <chasmonahtus@gmail.com>
2024-08-25 11:41:28 +0300

    v2

--- Modulle_2/modulle_2_hard.py
+++ Modulle_2/modulle_2_hard.py
@@ -1,16 +1,15 @@
 import random
 
 place = int(random.uniform(3, 20))
-result = []
-for i in range(1, place):
-    if place % i == 0 and place / i != 2:
+result = ''
+for i in range(place, 0, -1):
+    if place % i == 0 and place / i != 2 and place / i != 1:
         divider = int(place / i)
     else:
         continue
     for k in range(1, divider):
         if k < (divider / 2):
-            temp_value = [str(f'{divider - k}-{k}')]
-            result.extend(temp_value)
+            result += f'{k}+{divider - k} '
         else:
             break
 print(f'Число в первом поле {place}, ваш пароль: ')
